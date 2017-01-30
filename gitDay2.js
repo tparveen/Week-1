9c68b81397c3d9c03c00af9112fd394eb3164786
Tauhida Parveen <tauhida.parveen@gmail.com>
2017-01-30 10:45:16 -0500

    new files added

---
+++ gitDay2.js
@@ -0,0 +1,11 @@
+function day2(){
+    console.log("Testing Git");
+    console.log("Testing Git again");
+    console.log("Testing Git again");
+
+    console.log("Testing Git again");
+
+    console.log("Tauhida - Testing Git again");
+
+
+}
---
+++ gitDay3.js
@@ -0,0 +1,11 @@
+function day3(){
+    console.log("Testing Git");
+    console.log("Testing Git again");
+    console.log("Testing Git again");
+
+    console.log("Testing Git again");
+
+    console.log("Tauhida - Testing Git again");
+
+
+}
