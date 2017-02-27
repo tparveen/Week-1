246b95042623bb6c5d9972bd3f4feea90bfb8267
Tauhida Parveen <tauhida.parveen@gmail.com>
2017-02-27 10:50:27 -0500

    new file

---
+++ gitNewFile.js
@@ -0,0 +1,15 @@
+function day1(){
+    console.log("Testing Git");
+    console.log("Testing Git again");
+    console.log("Testing Git again");
+
+    console.log("Testing Git again***");
+
+    console.log("Tauhida - Testing Git again");
+    
+    console.log("Tauhida - Testing Git again");
+    console.log("Tauhida - Testing Git again");
+    console.log("Tauhida - Testing Git again");
+
+
+}
